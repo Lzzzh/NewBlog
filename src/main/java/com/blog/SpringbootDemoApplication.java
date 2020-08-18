b4d8f60c88fab42318d4901834e595897d8c541d
Lzzzh <sakura_xzs@outlook.com>
2020-08-18 23:55:36 +0800

    2020.8.18.

--- src/main/java/com/blog/SpringbootDemoApplication.java
+++ src/main/java/com/blog/SpringbootDemoApplication.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
